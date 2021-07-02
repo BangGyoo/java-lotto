d06a0784173ca00fe96872d4ca715235e6fe010f
김병규 <luck4241@joonggonara.co.kr>
2021-07-02 16:54:20 +0900

    feat : 병규 로또 등수 테스트 코드 정상 실행이 되도록 수정

--- src/main/java/com/banggyoo/lotto/domain/LottoRank.java
+++ src/main/java/com/banggyoo/lotto/domain/LottoRank.java
@@ -11,6 +11,9 @@ public enum LottoRank {
     SECOND(5),
     FIRST(6);
 
+    public static final String OUT_BOUND_MATCH_COUNT_ERROR_MESSAGE = "matchCount는 0보다 크거나 같고 6보다 작거나 같아야 합니다.";
+    public static final int LOTTO_MATCH_MINIMUM_COUNT = 0;
+    public static final int LOTTO_MATCH_MAXIMUM_COUNT = 6;
     private final int matchCount;
     private static final List<LottoRank> ENUM_LIST;
 
@@ -23,9 +26,17 @@ public enum LottoRank {
     }
 
     public static LottoRank calcRank(int currentMatchCount) {
+        invalidLottoMatchCount(currentMatchCount);
+
         return ENUM_LIST.stream()
                 .filter(lottoRank -> lottoRank.matchCount == currentMatchCount)
                 .findFirst().orElse(NOTHING);
+    }
+
+    private static void invalidLottoMatchCount(int currentMatchCount) {
+        if (currentMatchCount < LOTTO_MATCH_MINIMUM_COUNT || currentMatchCount > LOTTO_MATCH_MAXIMUM_COUNT) {
+            throw new IllegalArgumentException(OUT_BOUND_MATCH_COUNT_ERROR_MESSAGE);
+        }
     }
 
 }
