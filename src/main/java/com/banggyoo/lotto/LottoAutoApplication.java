9b55959dd2dc60076a72ad4d34390799fe79ee96
김병규 <luck4241@joonggonara.co.kr>
2021-08-11 18:56:33 +0900

    Feat : input 값 파싱 시 에러 발생할 경우 다시 재시작 하도록 수정

--- src/main/java/com/banggyoo/lotto/LottoAutoApplication.java
+++ src/main/java/com/banggyoo/lotto/LottoAutoApplication.java
@@ -6,6 +6,9 @@ import com.banggyoo.lotto.viewer.InputView;
 import com.banggyoo.lotto.viewer.ResultView;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
 public class LottoAutoApplication {
 
@@ -13,12 +16,30 @@ public class LottoAutoApplication {
         ResultView resultView = new ResultView();
         InputView inputView = new InputView();
         LottoStore lottoStore = new LottoStore(inputView);
-        Money money = lottoStore.createBuyMoney();
+        Money money = (Money) executeExceptionCaseLoop(() -> lottoStore.createBuyMoney());
         resultView.displayPurchaseLottoCount(money);
         Lottos buyAutoLottos = lottoStore.buyAutoLottos(money);
         resultView.displayBuyAutoLottos(buyAutoLottos);
-        WinningLotto winningLotto = lottoStore.createWinningLotto();
+        WinningLotto winningLotto = (WinningLotto) executeExceptionCaseLoop(() -> lottoStore.createWinningLotto());
         List<LottoRank> ranks = lottoStore.calcRanks(buyAutoLottos, winningLotto);
         resultView.displayAnalysis(ranks, money);
     }
+
+    private Object executeExceptionCaseLoop(Callable<Object> function) {
+        Object result;
+        do {
+            result = executeOrNull(function);
+        } while(Objects.isNull(result));
+        return result;
+    }
+
+    private Object executeOrNull(Callable<Object> function) {
+        try {
+            return function.call();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
 }
